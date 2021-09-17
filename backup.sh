cde7bf9817d07f3aa55485c7f2c48cc62ba9facc
fots <facundotomatis@gmail.com>
2021-09-17 20:28:02 -0300

    17 09 2021 20:28

--- backup.sh
+++ backup.sh
@@ -23,6 +23,8 @@ then
     cp -rT ~/Pictures /media/fots/backupfiles/Pictures
     echo "Progress 25%"
     cp -rT ~/Documents /media/fots/backupfiles/Documents
+    echo "Progress 27%"
+    cp -rT ~/Books /media/fots/backupfiles/Books
     echo "Progress 30%"
     find . -size +512M -printf "%f\n" > /tmp/exclude.txt
     rsync -av ~/facultad /media/fots/backupfiles/facultad --exclude-from=/tmp/exclude.txt --quiet
