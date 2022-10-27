25b168421831b96a8fdb8502236694858a7162c4
fots <facundotomatis@gmail.com>
2022-10-27 04:51:33 -0300

    now lsp signatures show in window

--- nvim/lua/user/lsp/lsp-signature.lua
+++ nvim/lua/user/lsp/lsp-signature.lua
@@ -17,13 +17,13 @@ local cfg = {
   -- This setting only take effect in insert mode, it does not affect signature help in normal
   -- mode, 10 by default
 
-  floating_window = false, -- show hint in a floating window, set to false for virtual text only mode
+  floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
 
-  floating_window_above_cur_line = false, -- try to place the floating above the current line when possible Note:
+  floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
   -- will set to true when fully tested, set to false will use whichever side has more space
   -- this setting will be helpful if you do not want the PUM and floating win overlap
   fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
-  hint_enable = true, -- virtual hint enable
+  hint_enable = false, -- virtual hint enable
   hint_prefix = icons.misc.Squirrel .. " ", -- Panda for parameter
   hint_scheme = "Comment",
   use_lspsaga = false, -- set to true if you want to use lspsaga popup
@@ -35,7 +35,7 @@ local cfg = {
     border = "rounded", -- double, rounded, single, shadow, none
   },
 
-  always_trigger = true, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
+  always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
 
   auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
   extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
