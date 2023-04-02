08125d14c5c6a0897ab0498848944a246290d98d
fots <facundotomatis@gmail.com>
2023-04-02 17:31:00 -0300

    update

--- nvim/lua/user/copilot.lua
+++ nvim/lua/user/copilot.lua
@@ -38,6 +38,5 @@ copilot.setup{
     cvs = false,
     ["."] = false,
   },
-  plugin_manager_path = vim.fn.stdpath("data") .. "/site/pack/packer",
   server_opts_overrides = {},
 }
---
+++ scripts/backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# backup pictures, documents, and videos to a external drive mounted at /mnt/backupfiles
+
+# set the date
+DATE=$(date +%Y-%m-%d)
+
+# set the backup directory
+BACKUPDIR=/mnt/backupfiles
+
+# set the source directory
+SOURCEDIR="pictures/ documents/ videos/"
+
+# set the backup file name
+BACKUPFILE=$BACKUPDIR/$DATE-backup.tar.gz
+
+# set the log file
+LOGFILE=$BACKUPDIR/$DATE-backup.log
+
+# create the backup
+tar -cvpzf "$BACKUPFILE" -C "$HOME" $SOURCEDIR 2> "$LOGFILE"
