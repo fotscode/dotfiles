4fecba761477f782e0d2addd42c4b275e85e2461
fots <facundotomatis@gmail.com>
2023-04-06 18:59:08 -0300

    added proper files and update script for sioyek

---
+++ sioyek/update_sioyek.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo cp /etc/sioyek/keys.config /etc/sioyek/keys.config.bak
+sudo cp /etc/sioyek/prefs.config /etc/sioyek/prefs.config.bak
+sudo cp ~/.config/sioyek/keys.config /etc/sioyek/keys.config
+sudo cp ~/.config/sioyek/prefs.config /etc/sioyek/prefs.config
