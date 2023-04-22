1d5ccba068931f04cb1cc7c5c1c430a169981153
fots <facundotomatis@gmail.com>
2023-04-22 19:28:08 -0300

    fixed nvim-tree open on setup

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -3,131 +3,135 @@
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-    vim.notify("NvimTree not installed")
-    return
+	vim.notify("NvimTree not installed")
+	return
 end
 
 local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
 if not config_status_ok then
-    return
+	return
 end
 
-local icons = require "user.icons"
+local icons = require("user.icons")
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
-
 local function notify_level(level)
-    return function(msg)
-        vim.schedule(function()
-            vim.api.nvim_echo({ { msg, "WarningMsg" } }, false, {})
-        end)
-    end
+	return function(msg)
+		vim.schedule(function()
+			vim.api.nvim_echo({ { msg, "WarningMsg" } }, false, {})
+		end)
+	end
 end
 
-nvim_tree.setup {
-    disable_netrw = true,
-    hijack_netrw = true,
-    hijack_cursor = false,
-    update_cwd = true,
-    diagnostics = {
-        enable = true,
-        icons = {
-            hint = icons.diagnostics.Hint,
-            info = icons.diagnostics.Information,
-            warning = icons.diagnostics.Warning,
-            error = icons.diagnostics.Error,
-        },
-    },
-    update_focused_file = {
-        enable = true,
-        update_cwd = true,
-        ignore_list = {},
-    },
-    system_open = {
-        cmd = 'xdg-open',
-        args = {},
-    },
-    filters = {
-        dotfiles = false,
-        custom = {},
-    },
-    git = {
-        enable = true,
-        ignore = true,
-        timeout = 500,
-    },
-    view = {
-        hide_root_folder = false,
-        side = "left",
-        mappings = {
-            custom_only = false,
-            list = {
-                { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-                { key = "h", cb = tree_cb "close_node" },
-                { key = "v", cb = tree_cb "vsplit" },
-            },
-        },
-        number = false,
-        relativenumber = false,
-    },
-    renderer = {
-        add_trailing = false,
-        group_empty = true,
-        highlight_git = false,
-        full_name = false,
-        highlight_opened_files = "none",
-        root_folder_modifier = ":~",
-        indent_markers = {
-            enable = false,
-            inline_arrows = true,
-            icons = {
-                corner = "└",
-                edge = "│",
-                item = "│",
-                none = " ",
-            },
-        },
-        icons = {
-            webdev_colors = true,
-            git_placement = "before",
-            padding = " ",
-            symlink_arrow = " ➛ ",
-            show = {
-                file = true,
-                folder = true,
-                folder_arrow = true,
-                git = true,
-            },
-            glyphs = {
-                default = "",
-                symlink = "",
-                folder = {
-                    arrow_open = icons.ui.ArrowOpen,
-                    arrow_closed = icons.ui.ArrowClosed,
-                    default = "",
-                    open = "",
-                    empty = "",
-                    empty_open = "",
-                    symlink = "",
-                    symlink_open = "",
-                },
-                git = {
-                    unstaged = "",
-                    staged = "S",
-                    unmerged = "",
-                    renamed = "➜",
-                    untracked = "U",
-                    deleted = "",
-                    ignored = "◌",
-                },
-            },
-        },
-        special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
-        symlink_destination = true,
-    },
-    trash = {
-        cmd = "trash",
-        require_confirm = true,
-    },
+nvim_tree_config.notify = {
+    { level = "error", msg = notify_level("error") },
+    { level = "warning", msg = notify_level("warning") },
 }
+
+nvim_tree.setup({
+	disable_netrw = false,
+	hijack_netrw = true,
+	hijack_cursor = false,
+	update_cwd = true,
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = icons.diagnostics.Hint,
+			info = icons.diagnostics.Information,
+			warning = icons.diagnostics.Warning,
+			error = icons.diagnostics.Error,
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	system_open = {
+		cmd = "xdg-open",
+		args = {},
+	},
+	filters = {
+		dotfiles = false,
+		custom = {},
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
+	view = {
+		hide_root_folder = false,
+		side = "left",
+		mappings = {
+			custom_only = false,
+			list = {
+				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
+				{ key = "h", cb = tree_cb("close_node") },
+				{ key = "v", cb = tree_cb("vsplit") },
+			},
+		},
+		number = false,
+		relativenumber = false,
+	},
+	renderer = {
+		add_trailing = false,
+		group_empty = true,
+		highlight_git = false,
+		full_name = false,
+		highlight_opened_files = "none",
+		root_folder_modifier = ":~",
+		indent_markers = {
+			enable = false,
+			inline_arrows = true,
+			icons = {
+				corner = "└",
+				edge = "│",
+				item = "│",
+				none = " ",
+			},
+		},
+		icons = {
+			webdev_colors = true,
+			git_placement = "before",
+			padding = " ",
+			symlink_arrow = " ➛ ",
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+			},
+			glyphs = {
+				default = "",
+				symlink = "",
+				folder = {
+					arrow_open = icons.ui.ArrowOpen,
+					arrow_closed = icons.ui.ArrowClosed,
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+					symlink_open = "",
+				},
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "U",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
+		special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+		symlink_destination = true,
+	},
+	trash = {
+		cmd = "trash",
+		require_confirm = true,
+	},
+})
