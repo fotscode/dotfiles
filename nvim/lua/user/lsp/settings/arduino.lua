96698cb3c9b68ac3a902008be86ed99a1eb946a0
fots <facundotomatis@gmail.com>
2023-04-27 22:51:11 -0300

    added home instead of full path

--- nvim/lua/user/lsp/settings/arduino.lua
+++ nvim/lua/user/lsp/settings/arduino.lua
@@ -1,8 +1,9 @@
 local DEFAULT_FQBN = "esp32:esp32:esp32"
+local HOME= os.getenv("HOME")
 return {
     cmd = {
         "arduino-language-server",
-        "-cli-config", "/home/fots/.arduino15/arduino-cli.yaml",
+        "-cli-config", HOME.."/.arduino15/arduino-cli.yaml",
         "-fqbn",
         DEFAULT_FQBN,
     },
