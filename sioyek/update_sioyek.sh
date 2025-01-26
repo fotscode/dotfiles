5fa04671b6950807ee38e511e54dabe564162a1c
fots <facundotomatis@gmail.com>
2025-01-26 16:04:49 -0300

    feat: added epub mimetype default config to sioyek

--- sioyek/update_sioyek.sh
+++ sioyek/update_sioyek.sh
@@ -3,3 +3,4 @@ sudo cp /etc/sioyek/keys.config /etc/sioyek/keys.config.bak
 sudo cp /etc/sioyek/prefs.config /etc/sioyek/prefs.config.bak
 sudo cp ~/.config/sioyek/keys.config /etc/sioyek/keys.config
 sudo cp ~/.config/sioyek/prefs.config /etc/sioyek/prefs.config
+xdg-mime default sioyek.desktop application/epub+zip
