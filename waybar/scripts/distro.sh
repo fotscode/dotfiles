b8896afbcaf7751917ec0c027f0e9783356c265f
fots <facundotomatis@gmail.com>
2025-10-22 23:53:45 -0300

    feat(waybar): minor improvements

--- waybar/scripts/distro.sh
+++ waybar/scripts/distro.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-echo '{
-  "text": "Uptime: '"$(uptime -p)"'",
-  "tooltip": "System info:\n'"$(uname -a)"'",
-  "class": ["info"],
-  "percentage": 75
-}'
+up="$(uptime -p | awk '
+{
+    for(i=2;i<=NF;i++){
+        if($i ~ /day/) printf "%dd ", $(i-1);
+        else if($i ~ /hour/) printf "%dh ", $(i-1);
+        else if($i ~ /minute/) printf "%dm ", $(i-1);
+    }
+    print ""
+}')"
+distro="$(neofetch --off| grep OS | cut -d' ' -f2- | sed -r 's/\x1B\[[0-9;]*[mK]//g')"
+kernel="$(neofetch --off| grep Kernel | cut -d' ' -f2- | sed -r 's/\x1B\[[0-9;]*[mK]//g')"
+
+echo "{
+  "$up"| "$distro"| "$kernel"
+}"
